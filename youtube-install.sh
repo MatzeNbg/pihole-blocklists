ab216d78c1115d61f28b91bc63c612ba71357e78
Matze <66997348+MatzeNbg@users.noreply.github.com>
2020-07-01 16:01:39 +0200

    Update youtube-install.sh

--- youtube-install.sh
+++ youtube-install.sh
@@ -2,4 +2,4 @@ cd ~
 git clone https://github.com/kboghdady/youTube_ads_4_pi-hole.git
 cd youTube_ads_4_pi-hole
 sudo chmod a+x youtube.sh
-sudo /home/pi/youTube_ads_4_pi-hole/youtube.sh >/dev/null 
+sudo ~/youTube_ads_4_pi-hole/youtube.sh >/dev/null 
